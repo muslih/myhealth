95c7b0764e63cc1940de6cbe3d2eb15bf4c6d952
Muslih <muslihzarth@gmail.com>
2020-06-22 06:27:42 +0800

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+STDOUT.puts 'Seeding database...'
+
+(1..10).each do
+  user = User.create!(
+    name: Faker::Name.unique.name,
+    email: Faker::Internet.unique.email,
+    username: Faker::Internet.unique.username,
+    password: '12345',
+    password_confirmation: '12345'
+  )
+
+  pp({ id: user.id, email: user.email, password: 'password' })
+end
+
+hospital_one = Hospital.create!(
+  name: "#{Faker::Address.city} Hospital",
+  address:"#{Faker::Address.street_address} #{Faker::Address.secondary_address}"
+)
+pp({id: hospital_one.id, name: hospital_one.name, address: hospital_one.address})
+
+hospital_two = Hospital.create!(
+  name: "#{Faker::Address.city} Hospital",
+  address:"#{Faker::Address.street_address} #{Faker::Address.secondary_address}"
+)
+pp({id: hospital_two.id, name: hospital_two.name, address: hospital_two.address})
+
+Hospital.all.each do |hospital|
+  doctor = Doctor.create!(
+    name: Faker::Name.unique.name,
+    start_time: "08:00",
+    end_time: "12:00",
+    hospital_id: hospital.id
+  ) 
+
+  pp({id: doctor.id, name: doctor.name, start_time: doctor.start_time, end_time: doctor.end_time})
+
+  doctor = Doctor.create!(
+    name: Faker::Name.unique.name,
+    start_time: "13:00",
+    end_time: "15:00",
+    hospital_id: hospital.id
+  ) 
+
+  pp({id: doctor.id, name: doctor.name, start_time: doctor.start_time, end_time: doctor.end_time})
+end
+
